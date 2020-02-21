72adfdfcff49fa01b7b21d1523c75961314fee83
sirogamiemacs <sirogamiemacs@gmail.com>
2020-02-21 11:26:30 +0900

    change test code

--- test/test.cpp
+++ test/test.cpp
@@ -5,35 +5,48 @@
 #include <algorithm>
 #include "../include/stopWatch.h"
 
+#ifndef LOG
+#define LOG(msg) std::cout << "[LOG] (" __FILE__ ":" << __LINE__ << ") from " << __func__ << "()\n    " << msg << "\n";
+#endif
+
 int main(int argc, char** argv)
 {
 	// make timer.
 	stopWatchController timer_con;
-	
 	timer_con.set_title_name("compare");
-	if (argc == 2)
-	{
-		timer_con.set_file_name(argv[1]);
-	} 
-	else if (argc > 2)
-	{
-		std::cerr << "コマンドラインの引数が多すぎます。" << std::endl;
-	}
 
+	if (argc < 2)
+	{
+		LOG("コマンドライン引数の数が少ないです。");
+		exit(-1);
+	} 
+	if (static_cast<string>(argv[1]) == static_cast<string>("each"))	
+		timer_con.set_what_plot(stopwatch::PLOT);
+	else if (static_cast<string>(argv[1]) == static_cast<string>("ave"))
+		timer_con.set_what_plot(stopwatch::BAR);
+	if (argc == 3)
+		timer_con.set_file_name(argv[2]);
+	if (argc >= 4)
+	{
+		LOG("コマンドライン引数の数が多すぎます。");
+		exit(-1);
+	}
 
 	std::string title1 = "for_each";
 	std::string title2 = "range_for";
 	std::string title3 = "counter";
+	std::string title4 = "at";
 	
 	unsigned const int FOR_EACH  = timer_con.new_timer(title1);
 	unsigned const int RANGE_FOR = timer_con.new_timer(title2);
 	unsigned const int COUNTER   = timer_con.new_timer(title3);
+	unsigned const int AT		 = timer_con.new_timer(title4);
 
 
 	std::vector<std::vector<int>> vecvec(1000, std::vector<int>(1000, 0));
-	unsigned const int times = 100;
+	unsigned const int times = 50;
 
-	for (int k = 0; k < times; k++)
+	for (int k = 0; k < times; ++k)
 	{
 		timer_con.start(FOR_EACH);
 		for_each(vecvec.begin(), vecvec.end(), [](vector<int>& vec)
@@ -46,7 +59,7 @@ int main(int argc, char** argv)
 		timer_con.lap(FOR_EACH);
 	}
 
-	for (int k = 0; k < times; k++)
+	for (int k = 0; k < times; ++k)
 	{
 		timer_con.start(RANGE_FOR);
 		for (vector<int>& vec : vecvec)
@@ -59,10 +72,11 @@ int main(int argc, char** argv)
 		timer_con.lap(RANGE_FOR);
 	}
 
-	for (int k = 0; k < times; k++)
+	for (int k = 0; k < times; ++k)
 	{
 		timer_con.start(COUNTER);
-		for (int i = 0; i < vecvec.size(); ++i) 
+		int n = vecvec.size();
+		for (int i = 0; i < n; ++i) 
 			{
 			for (int j = 0; j < vecvec[i].size(); ++j) 
 			{
@@ -71,4 +85,19 @@ int main(int argc, char** argv)
 		}
 		timer_con.lap(COUNTER);
 	}
+
+	for (int k = 0; k < times; ++k)
+	{
+		timer_con.start(AT);
+		int n = vecvec.size();
+		for (int i = 0; i < n; ++i) 
+			{
+			for (int j = 0; j < vecvec[i].size(); ++j) 
+			{
+				++vecvec.at(i).at(j);
+			}		
+		}
+		timer_con.lap(AT);
+	}
+
 }
